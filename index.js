b2e9d93d8f816eeb5bf1608fcabd751f1e15a43d
Jack Hilscher <Jack@Jacks-MacBook-Air.local>
2019-04-26 13:19:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Add your code here
+function submitData(name, email){
+  fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      name: name,
+      email: email
+    })
+   })
+   .then(function(response) {
+    return response.json();
+  })
+  .then(function(object) {
+
+    document.querySelector("body").innerHTML = `This guys name is ${object.name}. His private email is ${object.email}...`
+    document.querySelector("body").innerHTML = `${object.id}`
+
+      })
+      .catch(function(error){
+        alert("Unauthorized Access")
+        let body = document.querySelector("body")
+        let p = document.createElement('p')
+        p.innerHTML = `${"Unauthorized Access"}`
+        body.appendChild(p)
+      })
+}
+
+submitData("Ratman", "WayneManor@Gootham.org")
